75d5d6f54d4ee21cf575df1b669390be61841f9f
cfdoud <prouddoud01@gmail.com>
2023-04-22 02:11:10 -0700

    fb

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNy7i2T8InF960cgVGYpxyjd3bwWMtqSo",
+  authDomain: "waterhack-2ad1c.firebaseapp.com",
+  projectId: "waterhack-2ad1c",
+  storageBucket: "waterhack-2ad1c.appspot.com",
+  messagingSenderId: "509186396739",
+  appId: "1:509186396739:web:138783387c42a34cfc3af6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
