5566cb29cf208f4df36ec132c914627de9320c51
cfdoud <prouddoud01@gmail.com>
2023-04-22 01:37:53 -0700

    start

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import Water from "./screens/Water";
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* Rest of your app code */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Water"
+          component={Water}
+          options={{title: 'Water'}}
+        />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/Water.js
@@ -0,0 +1,33 @@
+import { StyleSheet,
+        Text,
+        View 
+    } from 'react-native';
+
+/*-------- Hook import --------*/
+import { useState } from 'react';
+
+
+
+/*-------- Variables --------*/
+
+function Water({navigation}){
+
+    return(
+        <View style = {{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+          <Text>Water</Text>
+          <Text>JOJOJOJOJOJOJOJOJOJOJOJOJOJOJOJ</Text>
+        </View>
+    );
+
+    }
+export default Water;
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#1f483',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+  
