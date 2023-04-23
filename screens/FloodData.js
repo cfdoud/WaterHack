0617fda9f1bebbe8a23c5971c99a23bc0e2f02b1
Uriel Barba <ubarba@ucmerced.edu>
2023-04-22 21:07:20 -0700

    supabase

--- screens/FloodData.js
+++ screens/FloodData.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import supabase from "../db/supabaseClient";
+
+async function getAttendee() {
+    const data = await supabase
+    .from('flow')
+    .select('692');
+    }
 
 
 function FloodData() {
+    getAttendee();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Flood Data</Text>
